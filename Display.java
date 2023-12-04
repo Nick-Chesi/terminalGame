159c0a9a5b8774c87e525f7ee6d046e9812cc260
Nick <chesinick@gmail.com>
2023-12-03 22:21:24 -0700

    Trying to fix text

--- Display.java
+++ Display.java
@@ -14,6 +14,7 @@ import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryStack.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
+
 public class Display {
 	 // The window handle
     private long window;
--- Utils/TextRenderer.java
+++ Utils/TextRenderer.java
@@ -1,25 +1,19 @@
 package terminalGame.Utils;
 
 import java.awt.Font;
-import static org.lwjgl.opengl.GL11.*;
 
 import org.newdawn.slick.TrueTypeFont;
 
 public class TextRenderer {
 	
 	private Font awtFont = new Font("Time New Roman", Font.PLAIN, 24);
-	private TrueTypeFont font;
+	private TrueTypeFont font = new TrueTypeFont(awtFont, true);
 	
 	public TextRenderer() {
-		awtFont = new Font("Time New Roman", Font.PLAIN, 24);
-		//font = new TrueTypeFont(awtFont, false);
 	}
 	
 	public void drawString(int x, int y, String text) {
-		font = new TrueTypeFont(awtFont, false);
-		glEnable(GL_BLEND);
 		font.drawString(x, y, text);
-		glDisable(GL_BLEND);
 	}
 	
 }
