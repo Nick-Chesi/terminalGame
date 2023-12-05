120dfc35fa26968216cc5754fdbb9195415ee76b
Nick <chesinick@gmail.com>
2023-12-04 20:28:11 -0700

    Re-did window

--- Main.java
+++ Main.java
@@ -2,19 +2,50 @@ package terminalGame;
 
 import terminalGame.Utils.TextRenderer;
 
-public class Main {
+public class Main implements Runnable {
+	public Thread game;
 	
 	private double gameVersion = 0.0;
-	private String gameName = "Epoch";
+	private static String gameName = "Epoch";
 	
 	private static Main m = new Main();
 	public TextRenderer textRender = new TextRenderer();
 	public Settings settings = new Settings();
+	
+	public static Window window;
 
 	public static void main(String[] args) {
 		m.debugPrint("Launching Game");
-        new Display().run();
+		new Main().start();
     }
+	
+	public void start() {
+		game = new Thread(this, gameName);
+		game.start();
+	}
+	
+	public static void init() {
+		window = new Window(1200, 800, gameName);
+		window.setBackgroundColor(1.0f, 0, 0);
+		window.create();
+	}
+	
+	public void run() {
+		init();
+		
+		while (!window.shouldClose()) {
+			update();
+			render();
+		} 
+	}
+	
+	private void update() {
+		window.update();
+	}
+	
+	private void render() {
+		window.swapBuffers();
+	}
 	
     public double getGameVersion() {
 		return gameVersion;
@@ -27,5 +58,4 @@ public class Main {
 	public void debugPrint(String message) {
 		System.out.println("[Epoch DEBUG]: " + message);
 	}
-
 }
--- Utils/TextRenderer.java
+++ Utils/TextRenderer.java
@@ -2,18 +2,17 @@ package terminalGame.Utils;
 
 import java.awt.Font;
 
-import org.newdawn.slick.TrueTypeFont;
-
 public class TextRenderer {
 	
 	private Font awtFont = new Font("Time New Roman", Font.PLAIN, 24);
-	private TrueTypeFont font = new TrueTypeFont(awtFont, true);
+	//private TrueTypeFont font = new TrueTypeFont(awtFont, true);
 	
 	public TextRenderer() {
+		// add to make different fonts possible
 	}
 	
 	public void drawString(int x, int y, String text) {
-		font.drawString(x, y, text);
+		//font.drawString(x, y, text);
 	}
 	
 }
